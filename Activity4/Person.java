18b86d0bbb825f93f08aaa9139004e5d7cd15d1d
Ceihiro <ceihiroadriatico@gmail.com>
2025-04-15 20:26:21 +0800

    Person

---
+++ Activity4/Person.java
@@ -0,0 +1,16 @@
+class Person {
+    // Variable
+    private String name;
+    private int id;
+
+    // Constructor 
+    public Person(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name : " + name);
+        System.out.println("Id   : " + id);
+    }
+}
