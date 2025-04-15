d9c942de8a8eead9608d3190b34d018ada5d510d
Ceihiro <ceihiroadriatico@gmail.com>
2025-04-15 10:53:37 +0800

    Vehicle

--- TestVehicle.java
+++ TestVehicle.java
@@ -1,3 +1,32 @@
 public class Vehicle{
+    // Private Attributes
+    private String brand;
+    private int speed;
+    private String fuelType;
+
+    // Constructor
+    public Vehicle(String brand, int speed, String fuelType) {
+        this.brand = brand;
+        this.speed = speed;
+        this.fuelType = fuelType;
+    }
+
+    // getter methods
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    // Display Method
+    public String displayInfo(){
+        System.out.println("");
+    }
 
 }
